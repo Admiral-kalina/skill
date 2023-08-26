f09a6067f85d865d2d5fdfb6f10fd55196ad5440
Kalinichenko <kalinamono@gmail.com>
2023-08-26 12:47:06 +0300

    add blog page v1

--- src/features/instagramSlice/instagramSlice.js
+++ src/features/instagramSlice/instagramSlice.js
@@ -1,5 +1,6 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit'
 import {instagramApi} from "../../api";
+import axios from "axios";
 
 const initialState = {
     instagramPosts: [],
@@ -11,7 +12,7 @@ export const fetchInstagramData = createAsyncThunk(
     'instagram/fetchInstagramData',
     async () => {
         console.log('FETCH')
-        const res = await instagramApi.get()
+        const res = await axios.get('https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption&&access_token=IGQWRQWFFXQVc0ZA1lWeGZANSXoyaUFWVU5PSmd0U0J3Nlcwb2Y5OUh3em1IVlluVWoyTEdfSG5CekY5c2RmMTh1OVE2Yng3YlA2cGxfdWp3NV95aU03SWVLR29kaHV6NkhvbTJzYWNLRWd5dzhNMFJwb2NNdGRQeG8ZD')
         console.log(res)
         return await res.data
     }
